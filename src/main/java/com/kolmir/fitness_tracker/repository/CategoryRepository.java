83d7ce9ecea9141cb2ddc7f12e24a9f1d1d1ee57
k0lm1r <andrej88k0t0v@gmail.com>
2025-12-17 10:45:08 +0300

    feat(repository): add all main repositories
    - Created CategoryRepository for category managing
    - Created UserRepository for user
    - Created WorkoutRepository for workout

---
+++ src/main/java/com/kolmir/fitness_tracker/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.kolmir.fitness_tracker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kolmir.fitness_tracker.models.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    
+}
---
+++ src/main/java/com/kolmir/fitness_tracker/repository/WorkoutRepository.java
@@ -0,0 +1,11 @@
+package com.kolmir.fitness_tracker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kolmir.fitness_tracker.models.Workout;
+
+@Repository
+public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+    
+}
