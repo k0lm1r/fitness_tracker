a94ac666024768ff60f2bd7c858ebad4a26b12f0
k0lm1r <andrej88k0t0v@gmail.com>
2025-12-21 15:49:03 +0300

    .

--- src/main/java/com/kolmir/fitness_tracker/models/Image.java
+++ src/main/java/com/kolmir/fitness_tracker/models/Image.java
@@ -1,7 +1,5 @@
 package com.kolmir.fitness_tracker.models;
 
-import java.nio.file.Path;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
